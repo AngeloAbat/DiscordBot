66b9a03607e49e9f4b8e89783844cfcba93d4b11
AngeloAbat <97714011+AngeloAbat@users.noreply.github.com>
2023-02-15 15:26:21 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const rant = require("./comments/rants.json").rantReplies
 const sexy = require("./comments/sexy.json").sexyReplies
 
 const { Client, IntentsBitField } = require(`discord.js`);
+const { Player, QueryType } 
 
 const client = new Client({
   intents: [
@@ -10,32 +11,42 @@ const client = new Client({
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildVoiceStates
   ],
 });
 
 
 client.on('ready', (c)=>{
-    console.log(`${c.user.tag} is ready!`)
+    console.log(`${c.user.tag} is ready`)
+    client.user.setActivity({
+        name: "🎶 | Music Time",
+        type: "Listening"
+    })
 })
 
 client.on('messageCreate', (message)=>{
-    function getRandomIntInclusive(maxi) {
-        min = Math.ceil(0);
-        max = Math.floor(maxi - 1);
-        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    function getRandomInt(maxi) {
+        if(maxi){
+            min = Math.ceil(0);
+            max = Math.floor(maxi - 1);
+            return Math.floor(Math.random() * (max - min + 1) + min); 
+        } else{
+            min = Math.ceil(0);
+            max = Math.floor(100);
+            return Math.floor(Math.random() * (max - min + 1) + min); 
+        }
       }
-
-    if(message.author.bot){
+    if(message.author.bot || !message.content.startsWith(prefix)){
         return
     }
 
     if(message.content == `${prefix}Rant` ||message.content == `${prefix}rant`){
-        message.reply(`${rant[getRandomIntInclusive(rant.length)].message}`)
+        message.reply(`${rant[getRandomInt(rant.length)].message}`)
     }
 
     // message.author.username === 'Śmìlę' && message.author.discriminator === '4512')
     if(message.content == `${prefix}Sexy` ||message.content == `${prefix}sexy`){
-        message.reply(`${sexy[getRandomIntInclusive(sexy.length)].message}`)
+        message.reply(`${sexy[getRandomInt(sexy.length)].message}`)
     }
 
     if(message.content == `${prefix}Kawaii` || message.content == `${prefix}kawaii`){
@@ -45,58 +56,36 @@ client.on('messageCreate', (message)=>{
     if(message.content == `${prefix}Rage` || message.content == `${prefix}rage`){
         message.reply(`https://64.media.tumblr.com/eaeff45895abd85a2530aeeacd8e6e63/tumblr_ndn73oBBRw1td6y6ho1_500.gif`)
     }
+
+    if(message.content == `${prefix}Roll` || message.content == `${prefix}roll`){
+        let number = getRandomInt()
+        let results = `**${number}** `
+
+        if(number === 100){
+            results += 'Go buy yourself a lotto ticket!'
+        } else if(number == 99){
+            results += `Super close!!`
+        } else if(number >= 98){
+            results += `Lucky Mfkr`
+        } else if(number >= 80){
+            results += `Lucky!`
+        } else if(number >= 60){
+            results += `Sheeeshh`
+        } else if(number >= 40){
+            results += 'Nice'
+        } else if(number >= 20){
+            results += `Okay`
+        } else if(number >= 10){
+            results += `Loser`
+        } else if(number === 0){
+            results += `Same thing as a 100`
+        } else{
+            results += `Unlucky mfkr`
+        }
+
+        message.reply(`You rolled a(n) ${results}`)
+    }
+    
 })
 
 client.login(token);
-
-// const fs = require("node:fs");
-// const path = require("node:path");
-// const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-// const { token } = require("./config.json");
-
-// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// client.commands = new Collection();
-// const commandsPath = path.join(__dirname, "commands");
-// const commandFiles = fs
-//   .readdirSync(commandsPath)
-//   .filter((file) => file.endsWith(".js"));
-
-// for (const file of commandFiles) {
-//   const filePath = path.join(commandsPath, file);
-//   const command = require(filePath);
-//   client.commands.set(command.data.name, command);
-// }
-
-// client.once(Events.ClientReady, () => {
-//   console.log("Ready!");
-// });
-
-// client.on(Events.InteractionCreate, async (interaction) => {
-//   if (!interaction.isChatInputCommand())
-//     console.log("didn't work no interaction");
-
-//   if (interaction.commandName === "ping") {
-//     await interaction.reply({ content: "Secret Pong!", ephemeral: true });
-//   }
-
-//   const command = client.commands.get(interaction.commandName);
-
-//   if (!command) return;
-
-//   try {
-//     await command.execute(interaction);
-//   } catch (error) {
-//     console.error(error);
-//     await interaction.reply({
-//       content: "There was an error while executing this command!",
-//       ephemeral: true,
-//     });
-//   }
-// });
-
-// client.on("ready", (event) => {
-//   console.log(`Bot name = ${event.user.tag}`);
-// });
-// client.login(token);
-
